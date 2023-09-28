a8be93d843d7863527a51c0e471d9dfe229211bd
Saurabh Singh <saurabh180387@gmail.com>
2023-09-28 17:02:45 +0530

    remaining changes

---
+++ __init__.py
@@ -0,0 +1,2 @@
+"""This is a Python based package intended to be deployed to act as
+file classifier and storage"""
--- read_classify_directory.py
+++ read_classify_directory.py
@@ -1,14 +1,26 @@
 import os
+import global_config.py
+    
+gdrive_files = []
+s3_files=[]
 
-all_files = list()
-all_dirs = list()
+def get_file_extensions(path):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            file_extension = os.path.splitext(file)[1]
+            if file_extension in gdrive_file_type:
+                gdrive_files.append(file)
+            elif  file_extension in  s3_file_images or file_extension in  s3_file_media :
+                s3_files.append(file)
+            
+    #return True
 
+global path
+extensions = get_file_extensions(path)
 
-# Iterate for each dict object in os.walk()
-for root, dirs, files in os.walk("mypath"):
-	# Add the files list to the all_files list
-	all_files.extend(files)
-	# Add the dirs list to the all_dirs list
-	all_dirs.extend(dirs)
+def send_to_cloud():
+    ##
+
+    
 
 
