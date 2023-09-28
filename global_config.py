a4faab01773311486659b2ec83fe4afaa1f7a891
Saurabh Singh <saurabh180387@gmail.com>
2023-09-28 17:44:24 +0530

    Update global_config.py

--- global_config.py
+++ global_config.py
@@ -1,3 +1,7 @@
+###Credentials missing
+# id, secret,bucket_id
+# google_drve_creds
+
 gdrive_file_type=['.doc','.txt']
 s3_file_images=['jpg','png','svg','webp']
 s3_file_media=['mp3','mp4','mpeg4','wmv','3gp','webm']
